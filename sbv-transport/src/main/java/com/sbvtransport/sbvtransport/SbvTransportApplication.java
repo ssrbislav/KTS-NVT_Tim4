dccdbefda1c40448dca579c609bd1fe9e1df57b2
Srbislav <srbo_m12@hotmail.com>
2018-11-04 02:07:01 +0100

    Initial commit - Startup project created

---
+++ sbv-transport/src/main/java/com/sbvtransport/sbvtransport/SbvTransportApplication.java
@@ -0,0 +1,12 @@
+package com.sbvtransport.sbvtransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbvTransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbvTransportApplication.class, args);
+	}
+}
---
+++ sbv-transport/src/test/java/com/sbvtransport/sbvtransport/SbvTransportApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sbvtransport.sbvtransport;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbvTransportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
