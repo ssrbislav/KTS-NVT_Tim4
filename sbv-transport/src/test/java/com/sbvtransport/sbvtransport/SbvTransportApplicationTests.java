e61ec6e6a1f373cd454b8c62572a7c5b5f12c40d
Vladimir Gajčin <djvlada03@gmail.com>
2019-01-05 21:18:00 +0100

    Suite expansion

--- sbv-transport/src/test/java/com/sbvtransport/sbvtransport/SbvTransportApplicationTests.java
+++ sbv-transport/src/test/java/com/sbvtransport/sbvtransport/SbvTransportApplicationTests.java
@@ -1,26 +1,24 @@
 package com.sbvtransport.sbvtransport;
 
+import com.sbvtransport.sbvtransport.controller.*;
+import com.sbvtransport.sbvtransport.repository.*;
+import com.sbvtransport.sbvtransport.service.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import com.sbvtransport.sbvtransport.controller.BusControllerTest;
-import com.sbvtransport.sbvtransport.controller.SubwayControllerTest;
-import com.sbvtransport.sbvtransport.controller.TrolleyControllerTest;
-import com.sbvtransport.sbvtransport.repository.BusRepositoryTest;
-import com.sbvtransport.sbvtransport.repository.SubwayRepositoryTest;
-import com.sbvtransport.sbvtransport.repository.TrolleyRepositoryTest;
-import com.sbvtransport.sbvtransport.service.BusServiceJUnitTest;
-import com.sbvtransport.sbvtransport.service.BusServiceTest;
-import com.sbvtransport.sbvtransport.service.SubwayServiceJUnitTest;
-import com.sbvtransport.sbvtransport.service.SubwayServiceTest;
-import com.sbvtransport.sbvtransport.service.TrolleyServiceJUnitTest;
-import com.sbvtransport.sbvtransport.service.TrolleyServiceTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ BusControllerTest.class, SubwayControllerTest.class, TrolleyControllerTest.class,
+@Suite.SuiteClasses({
+		// B
+		BusControllerTest.class, SubwayControllerTest.class, TrolleyControllerTest.class,
 		BusRepositoryTest.class, SubwayRepositoryTest.class, TrolleyRepositoryTest.class, BusServiceJUnitTest.class,
 		BusServiceTest.class, SubwayServiceJUnitTest.class, SubwayServiceTest.class, TrolleyServiceJUnitTest.class,
-		TrolleyServiceTest.class })
+		TrolleyServiceTest.class,
+		// V
+		LineControllerTest.class, LocationControllerTest.class, StationControllerTest.class,
+		LocationRepositoryTest.class, LocationRepositoryTest.class, StationRepositoryTest.class,
+		LineServiceTest.class, LocationServiceTest.class, StationServiceTest.class,
+		LocationServiceJUnitTest.class})
 public class SbvTransportApplicationTests {
 
 	@Test
