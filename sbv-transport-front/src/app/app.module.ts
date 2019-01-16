99c5d4cce4d4c131403e5ec96b03c449b7acf7c0
Bojana Corilic <bojanastrengthgirl@gmail.com>
2019-01-16 19:24:17 +0100

    change : delete popup

--- sbv-transport-front/src/app/app.module.ts
+++ sbv-transport-front/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './header/login/login.component';
 import { HeaderComponent } from './header/header.component';
-import {PopupModule} from 'ng2-opd-popup';
 import { RegistrationComponent } from './header/registration/registration.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './mainPage/mainPage.component';
@@ -88,7 +87,6 @@ const appRoutes: Routes = [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    PopupModule.forRoot(),
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
