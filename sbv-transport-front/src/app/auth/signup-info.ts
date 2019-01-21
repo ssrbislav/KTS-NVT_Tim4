a1cc68a7f58c4100e14986ca1b0c01ea9de3050f
Srbislav <srbo_m12@hotmail.com>
2019-01-21 17:16:08 +0100

    Setting auth.service for user authentication

---
+++ sbv-transport-front/src/app/auth/signup-info.ts
@@ -0,0 +1,12 @@
+export class SignUpInfo {
+
+    email: string;
+    username: string;
+    role: string[];
+    password: string;
+    first_name: string;
+    last_name: string;
+    address: string;
+    phone_number: string;
+    date_birth: string;
+}
