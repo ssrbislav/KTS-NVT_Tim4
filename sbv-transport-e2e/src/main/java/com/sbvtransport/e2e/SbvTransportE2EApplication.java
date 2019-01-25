61dc9b26df7300cb6e5c97c7d48b838c6d3224e1
Vladimir Gajčin <djvlada03@gmail.com>
2019-01-25 03:01:29 +0100

    E2E project added
    Incl. directories and runners.

---
+++ sbv-transport-e2e/src/main/java/com/sbvtransport/e2e/SbvTransportE2EApplication.java
@@ -0,0 +1,14 @@
+package com.sbvtransport.e2e;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbvTransportE2EApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbvTransportE2EApplication.class);
+  }
+
+}
