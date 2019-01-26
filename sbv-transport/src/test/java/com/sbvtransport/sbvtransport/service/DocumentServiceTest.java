d2290242184b1b45aee94ba31b5e8c67cc57aa7b
Bojana Corilic <bojanastrengthgirl@gmail.com>
2019-01-26 01:30:08 +0100

    Test:  change document model-names

---
+++ sbv-transport/src/test/java/com/sbvtransport/sbvtransport/service/DocumentServiceTest.java
@@ -0,0 +1,37 @@
+package com.sbvtransport.sbvtransport.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sbvtransport.sbvtransport.model.Bus;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+@Transactional
+@Rollback(value=true)
+public class DocumentServiceTest {
+	
+	@Autowired
+	private IDocumentService documentService;
+	
+	@Test
+	public void findAllTest() {
+		
+
+	}
+
+	
+	
+
+}
